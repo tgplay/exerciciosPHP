2eed3fbfb893a4bb4344263437ec37f45a111ebf
Tiago <tiago@wvtodoz.com.br>
2023-09-21 17:53:51 -0300

    treinando php

---
+++ avancando/testes.php
@@ -0,0 +1,14 @@
+<?php
+//valores simples armazenados no vetor array
+$array = [
+    1,2,3,4
+];
+
+//array associativos com chaves e valores
+$array = [
+    1 => 'um',
+    true => 'dois',
+    1.3 => 'tres'
+];
+
+echo '<pre>'; var_dump($array); '</pre>';
